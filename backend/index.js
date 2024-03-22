f346319c36d461f77ebe1c7cb06d6abed6cf2898
cristinamedrom <cristinamedranoroman@gmail.com>
2024-03-22 07:24:18 +0100

    puerto 3000 y cors

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
+const PORT = 3000;
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/usuarios', userRoutes);
 
-const PORT = process.env.PORT || 5173;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
